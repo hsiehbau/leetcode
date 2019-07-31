c7749616bec11afed43537434d76f7f41ed574c3
hsiehbau <pistar@foxmail.com>
2019-07-31 22:24:53 +0800

    几种方法综合

--- 3. 无重复字符的最长子串.js
+++ 3. 无重复字符的最长子串.js
@@ -66,6 +66,22 @@ var lengthOfLongestSubstring = function(s) {
 };
 
 
+
+var lengthOfLongestSubstring = function(s) {
+    var start = 0, maxLen = 0;
+    var map = new Map();
+  
+    for(var i = 0; i < s.length; i++) {
+        var ch = s[i];
+      
+        if(map.get(ch) >= start) start = map.get(ch) + 1;
+        map.set(ch, i);
+      
+        if(i - start + 1 > maxLen) maxLen = i - start + 1;
+    }
+  
+    return maxLen;
+  };
 //用集合
 var lengthOfLongestSubstring = function(s) {
     let seen = new Set();
