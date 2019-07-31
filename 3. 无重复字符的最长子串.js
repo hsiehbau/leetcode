c0a0ac3fce4f7e9a718aa2536a04f0cd5ab96851
hsiehbau <pistar@foxmail.com>
2019-07-31 22:23:30 +0800

    hashmap

--- 3. 无重复字符的最长子串.js
+++ 3. 无重复字符的最长子串.js
@@ -47,4 +47,25 @@ var lengthOfLongestSubstring = function(s) {
 
 //用hashmap优化
 
+var lengthOfLongestSubstring = function(s) {
+    let map = new Map();
+    let maxLength = 0;
+    let num = 0;
+    let stringArr = s.split('');
+    
+    for(let i = 0; i < stringArr.length; i++){
+        let char = stringArr[i];
+        let val = map.get(char);
+        
+        if(val !== undefined && num <= val ) num = val + 1;
+        map.set(char, i);
+        val = map.get(char);
+        if(i - num + 1 > maxLength) maxLength = i - num + 1;
+    }
+    return maxLength;
+};
+
+
+
+
  
